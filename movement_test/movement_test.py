86545000862b8508c050a17a34cbe945fda007b0
Christopher Cheng <chris.chengjk@gmail.com>
2023-01-01 15:06:43 +0800

    Added more ports to test all 4 wheels at once

--- movement_test/movement_test.py
+++ movement_test/movement_test.py
@@ -3,26 +3,44 @@ import time
 
 # Setup connections
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(22, GPIO.OUT)  # IN 1
-GPIO.setup(24, GPIO.OUT)  # IN 2
-GPIO.setup(26, GPIO.OUT)  # IN 3
-GPIO.setup(28, GPIO.OUT)  # IN 4
+for port in (21, 22, 23, 24, 35, 36, 37, 38):
+    GPIO.setup(port, GPIO.OUT)
+    GPIO.output(port, GPIO.LOW)
 
 for i in range(10):
+    GPIO.output(21, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(21, GPIO.LOW)
+
     GPIO.output(22, GPIO.HIGH)
-    time.sleep(0.25)
+    time.sleep(1.5)
     GPIO.output(22, GPIO.LOW)
 
+    GPIO.output(23, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(23, GPIO.LOW)
+
     GPIO.output(24, GPIO.HIGH)
-    time.sleep(0.25)
+    time.sleep(1.5)
     GPIO.output(24, GPIO.LOW)
 
-    GPIO.output(26, GPIO.HIGH)
-    time.sleep(0.25)
-    GPIO.output(26, GPIO.LOW)
+    GPIO.output(35, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(35, GPIO.LOW)
 
-    GPIO.output(28, GPIO.HIGH)
-    time.sleep(0.25)
-    GPIO.output(28, GPIO.LOW)
+    GPIO.output(36, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(36, GPIO.LOW)
+
+    GPIO.output(37, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(37, GPIO.LOW)
+
+    GPIO.output(38, GPIO.HIGH)
+    time.sleep(1.5)
+    GPIO.output(38, GPIO.LOW)
+
+for port in (21, 22, 23, 24, 35, 36, 37, 38):
+    GPIO.output(port, GPIO.LOW)
 
 GPIO.cleanup()
